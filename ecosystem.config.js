ae57ab3bb096672b0df14cfd8351d12e59457dcc
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-03 14:36:40 +0200

    feat: Include new bots

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,6 +16,41 @@ module.exports = {
     instances: 1,
     autorestart: true,
     max_memory_restart: '100M',
+  }, {
+    name: 'lyra',
+    script: 'lyra.js',
+    watch: true,
+    instances: 1,
+    autorestart: true,
+    max_memory_restart: '100M',
+  }, {
+    name: 'nimrod',
+    script: 'nimrod.js',
+    watch: true,
+    instances: 1,
+    autorestart: true,
+    max_memory_restart: '100M',
+  }, {
+    name: 'procyon',
+    script: 'procyon.js',
+    watch: true,
+    instances: 1,
+    autorestart: true,
+    max_memory_restart: '100M',
+  }, {
+    name: 'senet',
+    script: 'senet.js',
+    watch: true,
+    instances: 1,
+    autorestart: true,
+    max_memory_restart: '100M',
+  }, {
+    name: 'thoth',
+    script: 'thoth.js',
+    watch: true,
+    instances: 1,
+    autorestart: true,
+    max_memory_restart: '100M',
   }],
   pm2api: {
     port: 8000
