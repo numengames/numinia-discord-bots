1e0fdf45b7209ccc6845a2ea0b34246eec8d16f6
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-10 21:21:33 +0200

    feat: Remove boba

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,5 @@
 module.exports = {
   apps : [{
-    name: 'boba',
-    script: 'boba.js',
-    watch: true,
-    instances: 1,
-    autorestart: true,
-    max_memory_restart: '100M',
-    env: {
-      PM2_API_PORT: 8000
-    }
-  }, {
     name: 'gumala',
     script: 'gumala.js',
     watch: true,
