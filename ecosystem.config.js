49ab2cb80d86992003d9e400547a542eb568b5c9
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-22 17:46:41 +0200

    chore: Comment features

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,36 +2,36 @@ const path = require('path');
 
 module.exports = {
   apps: [
-    {
-      script: 'ts-node',
-      name: 'gumala',
-      args: 'src/bots/gumala/index.ts',
-    },
-    {
-      script: 'ts-node',
-      name: 'nimrod',
-      args: 'src/bots/nimrod/index.ts',
-    },
-    {
-      script: 'ts-node',
-      name: 'procyon',
-      args: 'src/bots/procyon/index.ts',
-    },
-    {
-      script: 'ts-node',
-      name: 'senet',
-      args: 'src/bots/senet/index.ts',
-    },
+    // {
+    //   script: 'ts-node',
+    //   name: 'gumala',
+    //   args: 'src/bots/gumala/index.ts',
+    // },
+    // {
+    //   script: 'ts-node',
+    //   name: 'nimrod',
+    //   args: 'src/bots/nimrod/index.ts',
+    // },
+    // {
+    //   script: 'ts-node',
+    //   name: 'procyon',
+    //   args: 'src/bots/procyon/index.ts',
+    // },
+    // {
+    //   script: 'ts-node',
+    //   name: 'senet',
+    //   args: 'src/bots/senet/index.ts',
+    // },
     {
       script: 'ts-node',
       name: 'senet-dungeon-world-master',
       args: 'src/bots/senet-dungeon-world-master/index.ts',
     },
-    {
-      script: 'ts-node',
-      name: 'thoth',
-      args: 'src/bots/thoth/index.ts',
-    },
+    // {
+    //   script: 'ts-node',
+    //   name: 'thoth',
+    //   args: 'src/bots/thoth/index.ts',
+    // },
     {
       watch: false,
       script: 'ts-node',
@@ -40,14 +40,14 @@ module.exports = {
       name: 'senet-dungeon-world-master-deploy-commands',
       args: 'src/bots/senet-dungeon-world-master/deploy-commands.ts',
     },
-    {
-      watch: false,
-      script: 'ts-node',
-      exec_mode: 'fork',
-      autorestart: false,
-      name: 'lyra-deploy-commands',
-      args: 'src/bots/lyra/deploy-commands.ts',
-    },
+    // {
+    //   watch: false,
+    //   script: 'ts-node',
+    //   exec_mode: 'fork',
+    //   autorestart: false,
+    //   name: 'lyra-deploy-commands',
+    //   args: 'src/bots/lyra/deploy-commands.ts',
+    // },
     {
       name: 'health-check',
       args: path.join(__dirname, 'src/server.ts'),
