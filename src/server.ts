01b2a6dfe6c982929412e7bd8e7143bf1e41cb20
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-23 15:40:02 +0200

    fix: Improve the server.ts file watching that all pm2 services are alive

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,45 @@
+import pm2, { ProcessDescription } from 'pm2';
 import express, { Request, Response } from 'express';
-
-const app = express();
 
 const PORT = 8000;
 
+const app = express();
+
 app.get('/health', (_req: Request, res: Response) => {
-  res.send();
+  pm2.connect((err: unknown) => {
+    if (err) {
+      console.error('Failed to connect to PM2:', err);
+      return res
+        .status(500)
+        .json({ status: 'error', message: 'Failed to connect to PM2' });
+    }
+
+    pm2.list((err: unknown, list) => {
+      pm2.disconnect();
+      if (err) {
+        console.error('Failed to list PM2 processes:', err);
+        return res
+          .status(500)
+          .json({ status: 'error', message: 'Failed to list PM2 processes' });
+      }
+
+      const filteredList = list.filter(
+        (proc) => proc.name !== 'senet-dungeon-world-master-deploy-commands',
+      );
+
+      const isHealthy = filteredList.every(
+        (proc: ProcessDescription) => proc.pm2_env?.status === 'online',
+      );
+
+      if (isHealthy) {
+        return res.send();
+      }
+
+      return res
+        .status(500)
+        .json({ status: 'error', message: 'Some processes are not online' });
+    });
+  });
 });
 
 app.listen(PORT, () => {
