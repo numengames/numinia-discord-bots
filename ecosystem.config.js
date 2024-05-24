9f467aed642635c297b6cac44b53211038c8d461
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-25 01:55:52 +0200

    no message

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,19 @@
 module.exports = {
   apps: [
     {
+      watch: false,
       name: 'lyra',
       script: 'ts-node',
       args: 'src/bots/lyra.ts',
     },
     {
+      watch: false,
       script: 'ts-node',
       name: 'senet-dungeon-world-master',
       args: 'src/bots/senet-dungeon-world-master.ts',
     },
     {
+      watch: false,
       script: 'ts-node',
       name: 'health-check',
       args: 'src/server.ts',
