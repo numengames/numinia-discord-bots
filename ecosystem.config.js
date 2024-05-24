f49b7b6e276fabe07e533ca84b91db32d9473b25
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-25 01:54:59 +0200

    no message

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,47 +1,15 @@
 module.exports = {
   apps: [
     {
-      watch: true,
       name: 'lyra',
       script: 'ts-node',
       args: 'src/bots/lyra.ts',
     },
-    // {
-    //   watch: true,
-    //   name: 'gumala',
-    //   script: 'ts-node',
-    //   args: 'src/bots/gumala.ts',
-    // },
-    // {
-    //   watch: true,
-    //   name: 'nimrod',
-    //   script: 'ts-node',
-    //   args: 'src/bots/nimrod.ts',
-    // },
-    // {
-    //   watch: true,
-    //   name: 'procyon',
-    //   script: 'ts-node',
-    //   args: 'src/bots/procyon.ts',
-    // },
-    // {
-    //   watch: true,
-    //   name: 'senet',
-    //   script: 'ts-node',
-    //   args: 'src/bots/senet.ts',
-    // },
     {
-      watch: true,
       script: 'ts-node',
       name: 'senet-dungeon-world-master',
       args: 'src/bots/senet-dungeon-world-master.ts',
     },
-    // {
-    //   watch: true,
-    //   name: 'thoth',
-    //   script: 'ts-node',
-    //   args: 'src/bots/thoth.ts',
-    // },
     {
       script: 'ts-node',
       name: 'health-check',
