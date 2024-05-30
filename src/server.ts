4335a17aa5e43421eb0c6f5be5eff4c1ed3453e4
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-30 16:40:09 +0200

    no message

--- src/server.ts
+++ src/server.ts
@@ -11,36 +11,37 @@ app.set('port', config.port);
 app.use(cors);
 
 app.get('/health', (_req: Request, res: Response) => {
-  pm2.connect((err: unknown) => {
-    if (err) {
-      console.error('Failed to connect to PM2:', err);
-      return res
-        .status(500)
-        .json({ status: 'error', message: 'Failed to connect to PM2' });
-    }
+  return res.status(200).send();
+  // pm2.connect((err: unknown) => {
+  //   if (err) {
+  //     console.error('Failed to connect to PM2:', err);
+  //     return res
+  //       .status(500)
+  //       .json({ status: 'error', message: 'Failed to connect to PM2' });
+  //   }
 
-    pm2.list((err: unknown, list) => {
-      pm2.disconnect();
-      if (err) {
-        console.error('Failed to list PM2 processes:', err);
-        return res
-          .status(500)
-          .json({ status: 'error', message: 'Failed to list PM2 processes' });
-      }
+  //   pm2.list((err: unknown, list) => {
+  //     pm2.disconnect();
+  //     if (err) {
+  //       console.error('Failed to list PM2 processes:', err);
+  //       return res
+  //         .status(500)
+  //         .json({ status: 'error', message: 'Failed to list PM2 processes' });
+  //     }
 
-      const isHealthy = list.every(
-        (proc: ProcessDescription) => proc.pm2_env?.status === 'online',
-      );
+  //     const isHealthy = list.every(
+  //       (proc: ProcessDescription) => proc.pm2_env?.status === 'online',
+  //     );
 
-      if (isHealthy) {
-        return res.status(200).send();
-      }
+  //     if (isHealthy) {
+  //       return res.status(200).send();
+  //     }
 
-      return res
-        .status(500)
-        .json({ status: 'error', message: 'Some processes are not online' });
-    });
-  });
+  //     return res
+  //       .status(500)
+  //       .json({ status: 'error', message: 'Some processes are not online' });
+  //   });
+  // });
 });
 
 app.listen(config.port, () => {
