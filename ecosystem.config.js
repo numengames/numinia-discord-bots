4b639fe4453934d0b0a611a1fef9f6ccf22808d6
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-30 17:08:54 +0200

    no message

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,33 +1,33 @@
 module.exports = {
   apps: [
-    // {
-    //   watch: true,
-    //   name: 'lyra',
-    //   log_type: 'json',
-    //   script: 'ts-node',
-    //   combine_logs: true,
-    //   args: 'src/bots/lyra.ts',
-    //   log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-    //   log_options: {
-    //     'awslogs-group': '/ecs/numinia-discord-bots-fargate-service',
-    //     'awslogs-region': 'your-aws-region',
-    //     'awslogs-stream-prefix': 'pm2-logs',
-    //   },
-    // },
-    // {
-    //   watch: true,
-    //   log_type: 'json',
-    //   script: 'ts-node',
-    //   combine_logs: true,
-    //   name: 'senet-dungeon-world-master',
-    //   log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-    //   args: 'src/bots/senet-dungeon-world-master.ts',
-    //   log_options: {
-    //     'awslogs-group': '/ecs/numinia-discord-bots-fargate-service',
-    //     'awslogs-region': 'your-aws-region',
-    //     'awslogs-stream-prefix': 'pm2-logs',
-    //   },
-    // },
+    {
+      watch: true,
+      name: 'lyra',
+      log_type: 'json',
+      script: 'ts-node',
+      combine_logs: true,
+      args: 'src/bots/lyra.ts',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      log_options: {
+        'awslogs-group': '/ecs/numinia-discord-bots-fargate-service',
+        'awslogs-region': 'your-aws-region',
+        'awslogs-stream-prefix': 'pm2-logs',
+      },
+    },
+    {
+      watch: true,
+      log_type: 'json',
+      script: 'ts-node',
+      combine_logs: true,
+      name: 'senet-dungeon-world-master',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      args: 'src/bots/senet-dungeon-world-master.ts',
+      log_options: {
+        'awslogs-group': '/ecs/numinia-discord-bots-fargate-service',
+        'awslogs-region': 'your-aws-region',
+        'awslogs-stream-prefix': 'pm2-logs',
+      },
+    },
     {
       watch: false,
       log_type: 'json',
