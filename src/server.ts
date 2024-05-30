f3d086d83d8dea6415b3806b3954230b1bb4dbcf
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-30 15:46:16 +0200

    chore: Test health endpoint

--- src/server.ts
+++ src/server.ts
@@ -6,37 +6,38 @@ import config from './config';
 const app = express();
 
 app.get('/health', (_req: Request, res: Response) => {
-  pm2.connect((err: unknown) => {
-    if (err) {
-      console.error('Failed to connect to PM2:', err);
-      return res
-        .status(500)
-        .json({ status: 'error', message: 'Failed to connect to PM2' });
-    }
+  res.status(200).send();
+  // pm2.connect((err: unknown) => {
+  //   if (err) {
+  //     console.error('Failed to connect to PM2:', err);
+  //     return res
+  //       .status(500)
+  //       .json({ status: 'error', message: 'Failed to connect to PM2' });
+  //   }
 
-    pm2.list((err: unknown, list) => {
-      pm2.disconnect();
-      if (err) {
-        console.error('Failed to list PM2 processes:', err);
-        return res
-          .status(500)
-          .json({ status: 'error', message: 'Failed to list PM2 processes' });
-      }
+  //   pm2.list((err: unknown, list) => {
+  //     pm2.disconnect();
+  //     if (err) {
+  //       console.error('Failed to list PM2 processes:', err);
+  //       return res
+  //         .status(500)
+  //         .json({ status: 'error', message: 'Failed to list PM2 processes' });
+  //     }
 
-      const isHealthy = list.every(
-        (proc: ProcessDescription) => proc.pm2_env?.status === 'online',
-      );
+  //     const isHealthy = list.every(
+  //       (proc: ProcessDescription) => proc.pm2_env?.status === 'online',
+  //     );
 
-      console.log('isHealthy', isHealthy);
-      if (isHealthy) {
-        return res.send();
-      }
+  //     console.log('isHealthy', isHealthy);
+  //     if (isHealthy) {
+  //       return res.send();
+  //     }
 
-      return res
-        .status(500)
-        .json({ status: 'error', message: 'Some processes are not online' });
-    });
-  });
+  //     return res
+  //       .status(500)
+  //       .json({ status: 'error', message: 'Some processes are not online' });
+  //   });
+  // });
 });
 
 app.listen(config.port, () => {
