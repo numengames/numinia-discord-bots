6b079e3107e8072a5c0448fcc66dbe0f79c20cf6
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-31 12:17:53 +0200

    no message

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,6 @@ module.exports = {
       script: 'ts-node',
       combine_logs: true,
       args: 'src/bots/lyra.ts',
-      max_memory_restart: '250M',
       log_options: {
         'awslogs-group': '/ecs/numinia-discord-bots-fargate-service',
         'awslogs-region': 'your-aws-region',
@@ -19,7 +18,6 @@ module.exports = {
       combine_logs: true,
       name: 'senet-dungeon-world-master',
       args: 'src/bots/senet-dungeon-world-master.ts',
-      max_memory_restart: '250M',
       log_options: {
         'awslogs-group': '/ecs/numinia-discord-bots-fargate-service',
         'awslogs-region': 'your-aws-region',
@@ -32,8 +30,6 @@ module.exports = {
       combine_logs: true,
       name: 'health-check',
       args: 'src/server.ts',
-      instances: 1,
-      max_memory_restart: '250M',
       log_options: {
         'awslogs-group': '/ecs/numinia-discord-bots-fargate-service',
         'awslogs-region': 'your-aws-region',
