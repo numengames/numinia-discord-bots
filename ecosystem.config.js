7908288d78bb960ace5f7bc5623f1d2f876e562f
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-31 13:23:43 +0200

    feat: Include some scripts & exec in different modes (cluster & fork)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,31 @@ module.exports = {
     {
       watch: true,
       name: 'lyra',
+      exec_mode: 'cluster',
       script: 'dist/bots/lyra.js',
     },
     {
+      watch: false,
+      exec_mode: 'fork',
+      name: 'numinia-lyra-commands',
+      script: 'dist/deploy-commands/numinia-lyra/index.js',
+    },
+    {
       watch: true,
+      exec_mode: 'cluster',
       name: 'senet-dungeon-world-master',
       script: 'dist/bots/senet-dungeon-world-master.js',
     },
     {
       watch: false,
+      exec_mode: 'fork',
+      name: 'senet-dungeon-world-master-commands',
+      script: 'dist/deploy-commands/senet-dungeon-world-master/index.js',
+    },
+    {
+      watch: false,
       name: 'health-check',
+      exec_mode: 'cluster',
       script: 'dist/server.js',
     },
   ],
