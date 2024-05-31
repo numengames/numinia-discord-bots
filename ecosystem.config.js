b45b5a96ce76a576b9322a39bf497f70c408f321
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-31 13:06:14 +0200

    feat: Upgrade the scripts to launch javascript instead of typescript

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,38 +3,17 @@ module.exports = {
     {
       watch: true,
       name: 'lyra',
-      script: 'ts-node',
-      combine_logs: true,
-      args: 'src/bots/lyra.ts',
-      log_options: {
-        'awslogs-group': '/ecs/numinia-discord-bots-fargate-service',
-        'awslogs-region': 'your-aws-region',
-        'awslogs-stream-prefix': 'pm2-logs',
-      },
+      script: 'dist/bots/lyra.js',
     },
     {
       watch: true,
-      script: 'ts-node',
-      combine_logs: true,
       name: 'senet-dungeon-world-master',
-      args: 'src/bots/senet-dungeon-world-master.ts',
-      log_options: {
-        'awslogs-group': '/ecs/numinia-discord-bots-fargate-service',
-        'awslogs-region': 'your-aws-region',
-        'awslogs-stream-prefix': 'pm2-logs',
-      },
+      script: 'dist/bots/senet-dungeon-world-master.js',
     },
     {
       watch: false,
-      script: 'ts-node',
-      combine_logs: true,
       name: 'health-check',
-      args: 'src/server.ts',
-      log_options: {
-        'awslogs-group': '/ecs/numinia-discord-bots-fargate-service',
-        'awslogs-region': 'your-aws-region',
-        'awslogs-stream-prefix': 'pm2-logs',
-      },
+      script: 'dist/server.js',
     },
   ],
 };
